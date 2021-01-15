5d3f134209e8a2c507617b40ea35ad896e68ab99
Cassandra Kornhiser <cvkornhiser@gmail.com>
2021-01-15 13:59:23 -0500

    After S3 bucket config

--- DM/settings.py
+++ DM/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/mediafiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
