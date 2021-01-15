eeb032fe547ad940c01e93f56c3c7613e2a2ac2d
Cassandra Kornhiser <cvkornhiser@gmail.com>
2021-01-15 16:31:33 -0500

    Updated S3 bucket

--- DM/settings.py
+++ DM/settings.py
@@ -142,13 +142,6 @@ USE_TZ = True
 # )
 # # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'directMessage/static'),
-]
-
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
@@ -157,12 +150,18 @@ STATICFILES_DIRS = [
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_PRELOAD_METADATA = True
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATIC_URL = 'https://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/static/'
+
+
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+STATIC_URL = 'https://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/media/'
+
 
 # AWS_LOCATION = 'static'
 
@@ -171,8 +170,18 @@ DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 # DEFAULT_FILE_STORAGE = 'DM.storage_backends.MediaStorage'
 # # if a user tries to uplaod a file with the same name as another file, cant have the file be overwritten
 # AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
-# AWS_S3_REGION_NAME = 'us-east-2'
+AWS_DEFAULT_ACL = None
+AWS_S3_REGION_NAME = 'us-east-2'
+# # MEDIA_URL = '/media/'
+# STATIC_ROOT = 'staticfiles'
+# # STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'directMessage/static'),
+# ]
+
+
+# MEDIA_URL = STATIC_URL + 'media/'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # Documentation
 
--- DM/urls.py
+++ DM/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', include("directMessage.urls"))]
-#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include("directMessage.urls"))] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
