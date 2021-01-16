1f4f4c3adeee95012b678fa62aaf683765a1f43c
Cassandra Kornhiser <cvkornhiser@gmail.com>
2021-01-16 09:38:36 -0500

    S3 setup for static and media

--- DM/settings.py
+++ DM/settings.py
@@ -146,25 +146,30 @@ USE_TZ = True
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
 
-
+# Allows access to the bucket
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_DEFAULT_ACL = None
-AWS_S3_REGION_NAME = 'us-east-2'
-AWS_PRELOAD_METADATA = True
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.us-east-2.amazonaws.com'
 
-STATICFILES_LOCATION = 'static'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
-STATIC_URL = 'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+# AWS_S3_REGION_NAME = 'us-east-2'
+# AWS_PRELOAD_METADATA = True
+# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+STATICFILES_LOCATION = 'static'
+
+
+# STATIC_URL = 'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 # STATIC_URL = 'https://' + AWS_STORAGE_BUCKET_NAME +
 # '.s3.us-east-2.amazonaws.com/static/'
 # https://dmcassbucket.s3.us-east-2.amazonaws.com/static/img/arrow.png
 
 MEDIAFILES_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 
 # AWS_LOCATION = 'static'
